d0e2fc00ab682fedb6c2ef02483cbcbcb60d6485
Michele Bianchi <jazzinghen@gmail.com>
2018-06-07 16:32:03 +0900

    Completed single variable cost computation

--- ex1/computeCost.m
+++ ex1/computeCost.m
@@ -6,16 +6,19 @@ function J = computeCost(X, y, theta)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
+% Compute the the entire outups by using matrix multiplication.
+% I didn't think that X was a [m, 2] matrix
+linear_output = X * theta
 
-
-
+% Compute cost using a single line XD
+J = (1/(2*m)) * sum((linear_output - y) .^ 2)
 
 % =========================================================================
 
