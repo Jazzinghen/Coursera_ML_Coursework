3442c73a6a6ddb058372904890e4bb1dc557e435
Jazzinghen <jazzinghen@gmail.com>
2018-06-16 18:14:34 +0900

    Created the plotting function

--- ex2/plotData.m
+++ ex2/plotData.m
@@ -1,29 +1,27 @@
 function plotData(X, y)
-%PLOTDATA Plots the data points X and y into a new figure 
-%   PLOTDATA(x,y) plots the data points with + for the positive examples
-%   and o for the negative examples. X is assumed to be a Mx2 matrix.
+  %PLOTDATA Plots the data points X and y into a new figure
+  %   PLOTDATA(x,y) plots the data points with + for the positive examples
+  %   and o for the negative examples. X is assumed to be a Mx2 matrix.
 
-% Create New Figure
-figure; hold on;
+  % Create New Figure
+  figure; hold on;
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Plot the positive and negative examples on a
-%               2D plot, using the option 'k+' for the positive
-%               examples and 'ko' for the negative examples.
-%
+  % ====================== YOUR CODE HERE ======================
+  % Instructions: Plot the positive and negative examples on a
+  %               2D plot, using the option 'k+' for the positive
+  %               examples and 'ko' for the negative examples.
+  %
 
+  % Find the positions of 0s and 1s...
+  % This function returns a vector of locations
+  found_ones = find(y==1);
+  found_zeroes = find(y==0);
 
+  % Plot points based on which category they fall into.
+  scatter(ex2data2(found_ones, 1), ex2data2(found_ones, 2), 'k', '+');
+  scatter(ex2data2(found_zeroes, 1), ex2data2(found_zeroes, 2), 'k', 'o');
+  % =========================================================================
 
-
-
-
-
-
-
-% =========================================================================
-
-
-
-hold off;
+  hold off;
 
 end
