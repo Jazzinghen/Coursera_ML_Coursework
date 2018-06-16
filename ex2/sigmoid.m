a73f50576b51f5ef0b91e72417779d937ac02c71
Jazzinghen <jazzinghen@gmail.com>
2018-06-16 18:14:55 +0900

    Completed the Sigmoid exercise

--- ex2/sigmoid.m
+++ ex2/sigmoid.m
@@ -1,18 +1,18 @@
 function g = sigmoid(z)
-%SIGMOID Compute sigmoid function
-%   g = SIGMOID(z) computes the sigmoid of z.
+  %SIGMOID Compute sigmoid function
+  %   g = SIGMOID(z) computes the sigmoid of z.
 
-% You need to return the following variables correctly 
-g = zeros(size(z));
+  % You need to return the following variables correctly
+  g = zeros(size(z));
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the sigmoid of each value of z (z can be a matrix,
-%               vector or scalar).
+  % ====================== YOUR CODE HERE ======================
+  % Instructions: Compute the sigmoid of each value of z (z can be a matrix,
+  %               vector or scalar).
 
+  % I guess I did a couple idiot things.
+  % This function computes the single-value sigmoid per each data point
+  % in the provided dataset
+  g = 1 ./ (1 + e.^(-z));
 
-
-
-
-% =============================================================
-
+  % =============================================================
 end
