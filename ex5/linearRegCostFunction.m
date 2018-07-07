93bfb202d1986d31d8db932e5607f0dcd599db39
Jazzinghen <jazzinghen@gmail.com>
2018-07-07 18:20:49 +0900

    Completed regularized linear regression cost function

--- ex5/linearRegCostFunction.m
+++ ex5/linearRegCostFunction.m
@@ -1,37 +1,37 @@
 function [J, grad] = linearRegCostFunction(X, y, theta, lambda)
-%LINEARREGCOSTFUNCTION Compute cost and gradient for regularized linear 
-%regression with multiple variables
-%   [J, grad] = LINEARREGCOSTFUNCTION(X, y, theta, lambda) computes the 
-%   cost of using theta as the parameter for linear regression to fit the 
-%   data points in X and y. Returns the cost in J and the gradient in grad
+  %LINEARREGCOSTFUNCTION Compute cost and gradient for regularized linear
+  %regression with multiple variables
+  %   [J, grad] = LINEARREGCOSTFUNCTION(X, y, theta, lambda) computes the
+  %   cost of using theta as the parameter for linear regression to fit the
+  %   data points in X and y. Returns the cost in J and the gradient in grad
 
-% Initialize some useful values
-m = length(y); % number of training examples
+  % Initialize some useful values
+  m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
+  % You need to return the following variables correctly
+  J = 0;
+  grad = zeros(size(theta));
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost and gradient of regularized linear 
-%               regression for a particular choice of theta.
-%
-%               You should set J to the cost and grad to the gradient.
-%
+  % ====================== YOUR CODE HERE ======================
+  % Instructions: Compute the cost and gradient of regularized linear
+  %               regression for a particular choice of theta.
+  %
+  %               You should set J to the cost and grad to the gradient.
+  %
 
+  % Compute predictions and then the deviations sum
+  predictions = X * theta;
+  deltas = sum((predictions - y) .^ 2);
 
+  % Compute theta^2 and remove the first
+  regularization = theta .^ 2;
+  regularization(1) = 0;
 
+  % Mix all together
+  J = (1 / (2*m)) * deltas + (lambda / (2*m)) * sum(regularization);
 
+  % =========================================================================
 
-
-
-
-
-
-
-
-% =========================================================================
-
-grad = grad(:);
+  grad = grad(:);
 
 end
