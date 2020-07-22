e341fbabf78f149d922c8ab762ab3ea41b65ea27
Jazzinghen <jazzinghen@gmail.com>
2020-07-23 02:08:48 +0900

    Completed cost function
    Took more than I thought just because I thought that:
    
    - X was not padded
    - Matrices have inverted sizes when compared to the course
    - I am stupid and I just forgot that J(theta) is the difference,
      while I was just computing the sum of computed ys

--- 2020/ex1/computeCost.m
+++ 2020/ex1/computeCost.m
@@ -6,16 +6,15 @@ function J = computeCost(X, y, theta)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
-J = 0;
-
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
+% Compute all the costs for each sample
+total_J = X * theta;
 
-
-
+% Compute the final cost
+J = sum((total_J - y).^2)/(2.*m)
 
 % =========================================================================
 
